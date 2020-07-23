122b8302da320676e82aab3f7c83572744213a6f
Kim heonil <iamheonil@gmail.com>
2020-07-23 21:48:04 +0900

    JavaFramework - AOP

--- Spring03_AOP/src/test03/MyAdvice.java
+++ Spring03_AOP/src/test03/MyAdvice.java
@@ -5,7 +5,13 @@ import org.aspectj.lang.JoinPoint;
 public class MyAdvice {
 
 	public void test(JoinPoint join) {
-		
+		System.out.println(join.getTarget().getClass());
+		System.out.println(join.getSignature().getName());
+		System.out.println("출석 카드를 찍는다");
+	}
+	
+	public void after(JoinPoint join) {
+		System.out.println("집에 간다 제발");
 	}
 	
 }
