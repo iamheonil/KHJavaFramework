dba858636de906063503e149b98d71c37d2180b2
Kim heonil <iamheonil@gmail.com>
2020-07-24 21:49:05 +0900

    Spring 기초와 레거시 프로젝트 작성

--- Spring04_MVC/src/main/java/member/controller/MemberController.java
+++ Spring04_MVC/src/main/java/member/controller/MemberController.java
@@ -1,12 +1,16 @@
 package member.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 // Controller Annotation 으로 해당 객체를 WebApplicationContext 에 등록,
 // @RequestParam, @RequestMapping 을 사용할 수 있다
 public class MemberController {
 
-	
+	@RequestMapping("/member/join.do")	
+	public void join() {
+		
+	}
 	
 }
